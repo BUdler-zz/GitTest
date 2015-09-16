15872b0fa5e83d7ca16bfe0e022fdbb6357f914e
Bert Udler <bsu3pa@virginia.edu>
2015-09-16 16:41:22 -0400

    ADD MY CHANGES!!!!

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,7 +2,9 @@
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Goodbye Git");
+                System.out.println("I don't know why you say goodbye, I say hello");
+                System.out.println("Hello Git");
 	}
 
 }
